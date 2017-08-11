8463b39b8e50de2e244880bebe415108579a956e
kareemsuhail <1kayesh@students.iugaza.edu.ps>
2017-08-11 16:00:05 +0300

    =reorgnaize files

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    secret:'ok48+359_4b4i^cw+!b%q5ipm=c3-!k3cont%klrw@*ee_8x44',
-    mailgun_key:'key-a3629c2b0b22d3e51c5e0fa65034e709',
-    mailgun_domain:'mailgun.genemedlab.net',
-    system_auth_mail:'wallet app <auth@kareemWallet.com>',
-    host:"www.walletApp.me"
-}
